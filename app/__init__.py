407f3d47049946f9eb13c5c168eadec4e042395f
Caven15 <cavenditch15@gmail.com>
2024-10-24 10:37:55 +0200

    mise en place de l'environnement

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.services.connect_db import db
+
+# Initialisation de l'application Flask
+app = Flask(__name__)
+
+# Vérifier la connexion à la base donnée
+try:
+    # Vérifier que la connexion à mongoDB est active
+    db.client.admin.command('ping') # Ping pour s'assurer que la connexion est reussie
+    print("--------------------")
+    print("Connexion MongoDB établie !!!")
+    print("--------------------")
+except Exception as error :
+    # En cas d'erreur de connexion, affiche un message d'erreur
+    print(f"Erreru de connexion à mongoDB : \n {error}")
