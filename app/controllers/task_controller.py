42aa097bd5ddec77b2d72e8d159bf03b8d552337
Caven15 <cavenditch15@gmail.com>
2024-10-25 10:28:09 +0200

    ajout update, getAll, Delete

--- app/controllers/task_controller.py
+++ app/controllers/task_controller.py
@@ -1,4 +1,6 @@
+from bson import ObjectId
 from flask import  request, jsonify
+from marshmallow import ValidationError
 from app.models.task_schema import TaskSchema
 from app.services.connect_db import db
 
@@ -7,6 +9,13 @@ from app.services.connect_db import db
 # CRUD
 
 # GetAll : Récupérer toute les tâches
+def get_all():
+    tasks = list(db['task'].find())
+    
+    task_schema = TaskSchema(many=True)
+    serialized_task = task_schema.dump(tasks)
+    
+    return jsonify(serialized_task), 200
 
 # Create : Ajouter une tâche
 def ajouter_tache():
@@ -26,7 +35,7 @@ def ajouter_tache():
     result = db['task'].insert_one(new_task)
     
     # Ajouter l'id MonhgoDb généré dans la réponse
-    new_task['id'] = str(result.inserted_id)
+    new_task['_id'] = str(result.inserted_id)
     
     # Sérialisation la nouvelle tâche pour la réponse en JSON
     task_serialized = task_schema.dump(new_task)
@@ -36,6 +45,43 @@ def ajouter_tache():
 
 # Update : Mise à jour d'une tache
 def update(id):
-    return jsonify(f"Id récupéré lors de l'update => {id}"), 200
+    
+    # Valider les données de mise à jour
+    task_schema = TaskSchema()
+    data = request.json
+    
+    try:
+        # Charger les données partiellement (ne pas obliger tout les champs)
+        task_schema.load(data, partial=True)
+    except ValidationError as error:
+        # Retourner une erreur 400 si validation échoue
+        return jsonify(error), 400
+
+    # Rechercher la tâche à mettre à jour par son objectId
+    task = db['task'].find_one({"_id": ObjectId(id)})
+    
+    if task:
+        # Mettre à jour la tâche dans mongoDB avec les nouvelles données
+        db['task'].update_one({"_id": ObjectId(id)}, {"$set" : data})
+        
+        # Récupérer la tâche mise à jour pour l'afficher dans la réponse
+        updated_task = db['task'].find_one({"_id": ObjectId(id)})
+        
+        # Sérialiser la tâche mise à jour
+        serialized_task = task_schema.dump(updated_task)
+        
+        # Renoie la tâche mise à jour sous forme de JSON avec un code 200 (Succès)
+        return jsonify(serialized_task), 200
+    else:
+        # Retourner une erreur 404 si la tâche n'existe pas
+        return {'message': f"Aucune tâche trouvé pour l'id ${id} "}, 404 
 
 # Delete : Suppression d'une tâche
+def delete(id):
+    
+    result = db['task'].delete_one({"_id": ObjectId(id)})
+    
+    if result.deleted_count > 0:
+        return {'message' : f"La tâche correspondant a l'id ({id}) à bien été supprimé !"}, 204
+    else:
+        return {'message' : f'Aucune tâche correspondant a {id}'}, 404
--- app/routes/task.py
+++ app/routes/task.py
@@ -1,11 +1,18 @@
 from app import app
 from app.controllers import task_controller
 
+@app.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    return task_controller.get_all()
 
 @app.route('/task/create', methods=['POST'])
 def create_task():
     return task_controller.ajouter_tache()
 
-@app.route('/task/update/<string:id>', methods=['POST'])
+@app.route('/task/udpate/<string:id>', methods=['POST'])
 def update_task(id):
     return task_controller.update(id)
+
+@app.route('/task/<string:id>', methods=['DELETE'])
+def delete_task(id):
+    return task_controller.delete(id)
